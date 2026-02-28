1640c21ae9719af85aca023c11a662d8805dafb4
ankitdoi--coder <ankitdoi82@gmail.com>
2026-02-28 16:07:54 +0530

    About Us, Contact Us, Disclaimer added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { Admin } from './admin/admin';
 import { AdminLogin } from './admin-login/admin-login';
 import { AuthGuard } from './Guards/auth.guard';
 import { Contactus } from './contactus/contactus';
+import { Aboutus } from './aboutus/aboutus';
+import { Disclaimer } from './disclaimer/disclaimer';
+
 
 
 export const routes: Routes = [
@@ -13,5 +16,7 @@ export const routes: Routes = [
     { path: 'admin-login', component: AdminLogin },
     { path: 'admin', component: Admin, canActivate: [AuthGuard] },
     { path: 'contactus', component: Contactus },
+    {path:'aboutus',component:Aboutus},
+    {path:'disclaimer',component:Disclaimer},
     { path: '**', redirectTo: '' }
 ];
