0f26c6afe78c97d822973079d97d9350f8fe38f8
ankitdoi--coder <ankitdoi82@gmail.com>
2026-03-01 11:28:42 +0530

    feat(Download page Side Search and Category movie Loader)

--- src/app/download-page/download-page.ts
+++ src/app/download-page/download-page.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { Movie } from '../Services/movie';// Ensure correct path
 import { environment } from '../../environments/environment';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from "@angular/forms";
+import { Moviez } from '../Models/Moviez';
 
 @Component({
   selector: 'app-download-page',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './download-page.html',
   styleUrls: ['./download-page.scss']
 })
@@ -15,8 +17,22 @@ export class DownloadPageComponent implements OnInit {
   movie: any = {};
   relatedMovies: any[] = [];
   apiUrl = environment.apiUrl;
+  searchFormGroup: any;
+  moviesList: Moviez[] | null = null;
+  movieId:number | null=null;
+  categories:any[]=[];
+  categoryMovieList:any[]=[];
+  categorySelected:boolean=false;
 
-  constructor(private route: ActivatedRoute, private movieService: Movie) {}
+  constructor(private route: ActivatedRoute,
+    private movieService: Movie,
+    private fb:FormBuilder,
+    private router:Router
+    ) { 
+      this.searchFormGroup=this.fb.group({
+        search:['']
+      })
+    }
 
   ngOnInit() {
     // Subscribe to paramMap to detect changes in the URL ID
@@ -28,29 +44,88 @@ export class DownloadPageComponent implements OnInit {
     });
 
     this.getAllMovies();
+    this.getCategory();
   }
 
   getMovieById(id: number) {
     this.movieService.getMovieById(id).subscribe({
-      next: (data:any) => {
+      next: (data: any) => {
         this.movie = data;
         // Scroll to the top of the page when a new movie loads
         window.scrollTo(0, 0);
       },
-      error: (err:any) => {
+      error: (err: any) => {
         console.error("Failed to load Movie", err);
       }
     });
+
   }
+
 
   getAllMovies() {
     this.movieService.getAllMovies().subscribe({
-      next: (data:any) => {
+      next: (data: any) => {
         this.relatedMovies = data.slice(0, 4);
       },
-      error: (err:any) => {
+      error: (err: any) => {
         console.error(err);
       }
     });
   }
+
+  submitSearch() {
+    if(this.searchFormGroup.invalid){
+      console.error("Form is invalid. Please check the following fields:");
+      Object.keys(this.searchFormGroup.controls).forEach(key => {
+        const controlErrors = this.searchFormGroup.get(key)?.errors;
+        if (controlErrors) {
+          console.log('Field: ' + key + ', Errors: ', controlErrors);
+        }
+      });
+      return;
+    }
+
+    const formValue=this.searchFormGroup.value;
+    
+    this.movieService.searchMovie(formValue.search).subscribe({
+      next: (data: Moviez[]) => {
+        console.log('Search results:', data);
+        this.moviesList = data; // Update the UI list
+      },
+      error: (err: any) => {
+        console.error('Error searching movie:', err);
+      }
+    });
+  }
+
+  getCategory(){
+    this.movieService.getCategories().subscribe({
+      next:(data:Moviez[])=>{
+        console.log('search result :',data);
+        this.categories=data;
+      },
+      error:(err:any) =>{
+        console.error('error fetching category',err);
+      }
+    })
+  };
+
+  searchByCategory(category:string){
+    if(!category){
+      this.categorySelected=false;
+      this.categoryMovieList=[];
+      return;
+    }
+    this.categorySelected=true;
+    this.movieService.searchMovie(undefined,category).subscribe({
+      next:(data:Moviez[])=>{
+        console.log("seached movies by category :",data);
+        this.categoryMovieList=data;
+      },
+      error:(err:any)=>{
+        console.log("error in searching Movies",err);
+      }
+    })
+  }
+
 }
